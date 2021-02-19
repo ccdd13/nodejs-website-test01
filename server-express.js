9c521cc4613d74059122d4ae615a29ea08572121
Restyled.io <commits@restyled.io>
2021-02-19 14:59:33 +0000

    Restyled by clang-format

--- server-express.js
+++ server-express.js
@@ -7,7 +7,7 @@ const exphbs = require("express-handlebars");
 
 const app = express();
 const hbs = exphbs.create({
-  /* config */
+    /* config */
 });
 
 app.engine("handlebars", hbs.engine);
@@ -16,9 +16,7 @@ app.enable("view cache");
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index", { cache: false });
-});
+app.get("/", (req, res) => { res.render("index", {cache : false}); });
 
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
