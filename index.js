1329a89e72c9639ecac65751d7d70e6a931e4713
Restyled.io <commits@restyled.io>
2021-04-07 22:12:40 +0000

    Restyled by prettier

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 
-"use strict"
+"use strict";
 
-import os from 'os'
-import{server} from './src/server.js'
+import os from "os";
+import { server } from "./src/server.js";
 
-console.log('my nodejs web project')
-console.log(`\tos.platform() ${os.platform()}`)
-console.log(`\tos.arch() ${os.arch()}`)
-console.log(`\tos.type() ${os.type()}`)
-console.log(`\tos.release() ${os.release()}`)
-console.log(`\tos.version() ${os.version()}`)
-console.log(`\tprocess.version ${process.version}\n`)
+console.log("my nodejs web project");
+console.log(`\tos.platform() ${os.platform()}`);
+console.log(`\tos.arch() ${os.arch()}`);
+console.log(`\tos.type() ${os.type()}`);
+console.log(`\tos.release() ${os.release()}`);
+console.log(`\tos.version() ${os.version()}`);
+console.log(`\tprocess.version ${process.version}\n`);
 
-server.listen()
+server.listen();
